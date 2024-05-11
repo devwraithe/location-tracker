2c35d9da4d5d8141bfe65129c9e551e08f0ece65
ibrahim-ibrahim <folahanmi0001@gmail.com>
2024-05-11 21:51:27 +0100

    docs: add comments to main test file

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,24 +1,17 @@
-import 'dart:io';
-
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hive_flutter/adapters.dart';
 import 'package:location_tracker/app/app.dart';
-import 'package:location_tracker/app/modules/current_location/domain/location_adapter.dart';
 import 'package:location_tracker/app/shared/services/di_service.dart' as di;
-import 'package:path_provider/path_provider.dart';
 
 void main() {
   testWidgets('Should initialize the app', (tester) async {
+    // Ensure the test environment is initialized
     TestWidgetsFlutterBinding.ensureInitialized();
 
+    // Initialize the dependency injector
     di.init();
 
-    // Initialize hive and register adapters - for offline support
-    Directory dir = await getApplicationDocumentsDirectory();
-    await Hive.initFlutter(dir.path);
-    Hive.registerAdapter(LocationAdapter());
-
+    // Initialize screen util
     await ScreenUtil.ensureScreenSize();
 
     await tester.pumpWidget(
